5403a9a01fcc12a04752fe40e5cfad51696f750a
vishal_M <vellimuthu9@gmail.com>
2023-12-10 12:38:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.6 MB (85.01%)

---
+++ 0898-transpose-matrix/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int matrix1[][]=new int[matrix[0].length][matrix.length];
+        for (int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                matrix1[j][i]=matrix[i][j];
+            }
+
+        }
+        return matrix1;
+    }
+}
