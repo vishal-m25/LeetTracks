5b23866e1feb3b22a944f8f8e5bae205f707a2af
vishal_M <vellimuthu9@gmail.com>
2024-02-28 15:03:29 +0000

    [LeetCode Sync] Runtime - 5 ms (28.56%), Memory - 45.8 MB (83.81%)

---
+++ 0004-median-of-two-sorted-arrays/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n=(nums1.length)+(nums2.length);
+        int[] ar=new int[n];
+        System.arraycopy(nums1, 0, ar, 0, nums1.length); 
+        System.arraycopy(nums2, 0, ar, nums1.length, nums2.length);
+        Arrays.sort(ar);
+        if (n==1){
+            return(ar[0]);
+        }
+        if (n%2==0){
+            return ((ar[n/2]+ar[(n/2)-1])/2.0);
+        }
+        else{
+            return ar[(n/2)];
+        }
+    }
+}
