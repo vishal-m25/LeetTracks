19d14892e7c10cdee7f20919a0001fc46de5fca0
vishal_M <vellimuthu9@gmail.com>
2024-12-30 08:56:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.5 MB (17.25%)

--- 0908-middle-of-the-linked-list/solution.java
+++ 0908-middle-of-the-linked-list/solution.java
@@ -10,19 +10,12 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        if(head.next == null)
-            return head;
-        ListNode temp = head;
-        int t = 0;
-        while(temp.next != null){
-            temp = temp.next;
-            t++;
+        ListNode  fast=head;
+        ListNode slow=head;
+        while(fast!=null &&fast.next !=null){
+            fast=fast.next.next;
+            slow=slow.next;
         }
-        t = (t + 1) / 2;
-        while(t > 0){
-            head = head.next;
-            t--;
-        }
-        return head;
+        return slow;
     }
 }
