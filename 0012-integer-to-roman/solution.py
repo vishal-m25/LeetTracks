ad128d5663a1f7a66e6faf987843f4292da1338a
vishal_M <vellimuthu9@gmail.com>
2025-01-07 13:56:35 +0000

    [LeetCode Sync] Runtime - 3 ms (88.60%), Memory - 12.4 MB (17.31%)

--- 0012-integer-to-roman/solution.py
+++ 0012-integer-to-roman/solution.py
@@ -1,14 +1,17 @@
-class Solution(object):
+class Solution:
     def intToRoman(self, num):
-        """
-        :type num: int
-        :rtype: str
-        """
-        d={1:"I",5:"V",10:"X",50:"L",100:"C",500:"D",1000:"M",900:"CM",90:"XC",4:"IV",9:"IX",40:"XL",400:"CD"}
-        l=[1000,900,500,400,100,90,50,40,10,9,5,4,1]
-        s=""
-        for i in l:
-            while num>=i:
-                num-=i
-                s+=d[i]
-        return s
+        num_map = {
+            1: "I",
+            5: "V",    4: "IV",
+            10: "X",   9: "IX",
+            50: "L",   40: "XL",
+            100: "C",  90: "XC",
+            500: "D",  400: "CD",
+            1000: "M", 900: "CM",
+        }
+        r = ''
+        for n in [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]:
+            while n <= num:
+                r += num_map[n]
+                num-=n
+        return r
